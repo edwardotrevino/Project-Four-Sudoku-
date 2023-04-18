1361998f2676df2b6d963dd66c7929a1867d2a1d
Edwardo <edwardotrevino@ufl.edu>
2023-04-17 21:19:07 -0400

    Let me know if anything seems off or needs explaining :)

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,61 @@
 
+class SudokuGenerator:
 
+    def __init__(self, removed_cells):
+        self.row_length = 9
+        self.removed_cells = removed_cells
 
+    def get_board(self):
+        return [["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                ["-", "-", "-", "-", "-", "-", "-", "-", "-"]]
 
+    def print_board(self):
+        for row in self.get_board()[0]:  # row: ["-", "-", "-", "-", "-", "-","-", "-", "-"]
+            for col in self.get_board()[0]:
+                print(col, end=" ")
+            print()
 
+    def valid_in_row(self, row, num):  # row = self.get_board[row#]
+        if num in self.get_board()[row]:
+            return False
+        return True
 
+    def valid_in_col(self, col, num):  # for loop to iterate through each value of row, then col = self.get_board[row(i)][col(input)]
+        for row in range(len(self.get_board())):
+            if num in self.get_board()[row][col]:
+                return False
+        return True
 
+    def valid_in_box(self, row_start, col_start, num):
+        pass
 
+    def is_valid(self, row, col, num):
+        pass
 
+    def fill_box(self, row_start, col_start):
+        pass
 
+    def fill_diagonal(self):
+        pass
 
+    def fill_remaining(self, row, col):
+        pass
 
+    def fill_values(self):
+        pass
 
+    def remove_cells(self):
+        pass
 
-
+    def generate_sudoku(size, removed):
+        pass
 
 
 
