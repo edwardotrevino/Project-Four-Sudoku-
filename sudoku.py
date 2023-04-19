4d0ed42c0567e65a32cbebdaa4661c6930890bcf
Edwardo <edwardotrevino@ufl.edu>
2023-04-18 20:38:18 -0400

    Ended up finishing is_valid and switching the boolean values around. also added fill_box and fill_diagonal. let me know if anything needs clarification

--- sudoku.py
+++ sudoku.py
@@ -1,26 +1,28 @@
+import random
+
 
 class SudokuGenerator:
 
     def __init__(self, removed_cells):
         self.row_length = 9
         self.removed_cells = removed_cells
-
+        self.board = [["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
+                      ["-", "-", "-", "-", "-", "-", "-", "-", "-"]]
     def get_board(self):
-        return [["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-                ["-", "-", "-", "-", "-", "-", "-", "-", "-"]]
+        return self.board
 
     def print_board(self):
-        for row in self.get_board()[0]:  # row: ["-", "-", "-", "-", "-", "-","-", "-", "-"]
-            for col in self.get_board()[0]:
-                print(col, end=" ")
-            print()
+        for row in self.board:  # iterate over each row in the board
+            for col in row:  # iterate over each column in the current row
+                print(col, end=" ")  # print the current cell, followed by a space
+            print()  # move to the next line after each row has been printed
 
     def valid_in_row(self, row, num):  # row = self.get_board[row#]
         if num in self.get_board()[row]:
@@ -61,8 +63,8 @@ class SudokuGenerator:
         for row in range(row_start, row_start + 3):
             for col in range(col_start, col_start + 3):
                 if self.get_board()[row][col] == num:
-                    return True
-        return False
+                    return False
+        return True
 
         # loops through rows from the specified row_start to row_start + 2
         # and cols from col_start to col_start + 2, to determine if num is in a given 3x3 box
@@ -70,20 +72,48 @@ class SudokuGenerator:
 
 
     def is_valid(self, row, col, num):
-        if not self.valid_in_box(row, col, num):
+        if self.get_board()[row][col] != "-":
+            return False
+        if self.valid_in_box(row, col, num) and self.valid_in_row(row, num) and self.valid_in_col(col, num):
             return True
         return False
 
-        # if the num is already in the 3x3 box, then is_valid returns False, if not, is_valid returns True
-
+        # if the num is already in the 3x3 box or the specified row or col; then is_valid returns False, if not, is_valid returns True
         # not sure if this method should return false if user tries to change randomly generated numbers
         # or if that is determined in a different method, if not this should work
 
+        # edit(Eddie), im assuming so because in the game you shouldn't be able to overwrite any cells
     def fill_box(self, row_start, col_start):
-        pass
+        # Adjust row_start to the first row of the corresponding 3x3 box
+        if 0 <= row_start <= 2:
+            row_start = 0
+        elif 3 <= row_start <= 5:
+            row_start = 3
+        elif 6 <= row_start <= 8:
+            row_start = 6
+
+        # Adjust col_start to the first column of the corresponding 3x3 box
+        if 0 <= col_start <= 2:
+            col_start = 0
+        elif 3 <= col_start <= 5:
+            col_start = 3
+        elif 6 <= col_start <= 8:
+            col_start = 6
+
+        box_values = list(range(1, 10))  # Create a list of the numbers 1 to 9
+        random.shuffle(box_values)  # Shuffle the list to get a random order of the numbers
+
+        # Loop through each cell in the 3x3 box and assign a value
+        for row in range(row_start, row_start + 3):
+            for col in range(col_start, col_start + 3):
+                value = box_values.pop()  # Get the last value from the shuffled list and remove it
+                self.get_board()[row][col] = value  # Assign the value to the cell
 
     def fill_diagonal(self):
-        pass
+        diagonal_box_indices = [0, 3, 6]
+        for i in diagonal_box_indices:
+            self.fill_box(i,i)
+
 
     def fill_remaining(self, row, col):
         pass
@@ -98,5 +128,6 @@ class SudokuGenerator:
         pass
 
 
-
-
+sudoku = SudokuGenerator(0)
+print(sudoku.fill_box(0, 0))
+sudoku.print_board()
